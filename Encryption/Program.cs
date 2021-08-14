d08934eb59e640b4849e330bb5bb18567a932a5f
Ahmed Dewidar <ahmed.mahmoud18498@yahoo.com>
2021-08-14 04:11:27 +0200

    Encryption and Decryption

---
+++ Encryption/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "dewi";
+            GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+            EncryptionManager E = new EncryptionManager();
+            E.FileEncrypt(@"C:\Users\Dev-Lap-Dewidar\Encryption\test.mp3", password);
+            gch.Free();
+
+        }
+    }
+}
